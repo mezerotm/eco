7e7be1eb32feeb413d3dd2a566aee29f1807470d
Carlos Rincon <mezerotm@gmail.com>
2017-09-05 02:38:11 -0400

    changed connection to pool

--- src/database/connect.js
+++ src/database/connect.js
@@ -2,16 +2,16 @@
  * Created by Carlos on 6/29/2017.
  */
 const mysql = require('mysql');
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
 	host: 'us-cdbr-iron-east-05.cleardb.net',
 	user: 'b74060db1ae5a3',
 	password: '2362c679',
 	database: 'heroku_0367e03a8c5429a'
 });
 
-connection.connect((err) =>{
-	if(err) return console.error(`error connecting: ${err}`);
-	console.log('connected database at 3306');
-});
+//connection.connect((err) =>{
+//	if(err) return console.error(`error connecting: ${err}`);
+//	console.log('connected database at 3306');
+//});
 
 module.exports = connection;
