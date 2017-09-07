aceb6062c98742e5be85320868a4d1b18e2f1590
Carlos Rincon <mezerotm@gmail.com>
2017-09-07 01:08:15 -0400

    Added record/create

--- src/routes/record/create/index.js
+++ src/routes/record/create/index.js
@@ -4,12 +4,12 @@ const connection = require(process.cwd() + '/src/database/connect.js');
 
 router.get('/', (req, res, next) => {
 	let device_id = req.params.device_id;
-	let data = req.params.data;
+	let liter_val = req.params.liter_val;
 
 	if(!device_id || !data)
 		return res.send(412, 'Precondition Failed');
 
-	connection.query(`CALL create_record('${device_id}', '${data}')`, (err, results, fields) => {
+	connection.query(`CALL create_record('${device_id}', '${liter_val}')`, (err, results, fields) => {
 		if(err) return res.send(404, 'Not Found');
 		return res.send(201, 'Created');
 	});
