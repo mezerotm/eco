6dee67f190bc2bf6ad224d77eff293bd80ae9fa4
Carlos Rincon <mezerotm@gmail.com>
2017-09-07 01:36:54 -0400

    Added production

--- src/database/connect.js
+++ src/database/connect.js
@@ -1,12 +1,14 @@
 /**
  * Created by Carlos on 6/29/2017.
  */
+const production = require('./production.js');
 const mysql = require('mysql');
+
 const connection = mysql.createPool({
-	host: 'us-cdbr-iron-east-05.cleardb.net',
-	user: 'b74060db1ae5a3',
-	password: '2362c679',
-	database: 'heroku_0367e03a8c5429a'
+	host: production.host,
+	user: production.user,
+	password: production.password,
+	database: production.database
 });
 
 module.exports = connection;
