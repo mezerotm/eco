aebf3b7e77a0a850cd6decd2f3158d1228bd8c42
Carlos Rincon <mezerotm@gmail.com>
2017-09-07 00:36:16 -0400

    added more routes

--- src/routes/record/create/index.js
+++ src/routes/record/create/index.js
@@ -1,6 +1,5 @@
 const Router = require('restify-router').Router;
 const router = new Router();
-const sjcl = require('sjcl');
 const connection = require(process.cwd() + '/src/database/connect.js');
 
 router.get('/', (req, res, next) => {
@@ -15,3 +14,5 @@ router.get('/', (req, res, next) => {
 		return res.send(201, 'Created');
 	});
 });
+
+module.exports = router;
--- src/routes/users/index.js
+++ src/routes/users/index.js
@@ -3,7 +3,6 @@
  */
 const Router = require('restify-router').Router;
 const router = new Router();
-const sjcl = require('sjcl');
 const connection = require(process.cwd() + '/src/database/connect.js');
 
 router.get('/', (req, res, next) =>{
